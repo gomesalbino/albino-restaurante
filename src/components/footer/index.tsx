33624ff59bc9255122ce0878f09cdc15a935dd25
Albino Gomes <gomesalbino26@outlook.com>
2023-03-12 12:11:13 -0300

    Adicionando a modificação do no logo

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as Logo } from 'assets/img/logo.svg';
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-        <Logo />
+        <Logo className={styles.logo}/>
         </footer>
  );
 }
